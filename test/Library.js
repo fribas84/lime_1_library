edb235c5e3b116450921b480e233dac339c26fb6
Fernando Ribas <fribas84@gmail.com>
2022-10-04 13:54:50 -0300

    Fixed cannot borrow if borrow tests

--- test/Library.js
+++ test/Library.js
@@ -5,7 +5,7 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-describe("Library", function () {
+describe("Library Contract basic funcionality", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
@@ -40,7 +40,7 @@ describe("Library", function () {
 
     it("Not-owner cannot add books to stock", async function () {
       const { library,otherAccount1 } = await loadFixture(deployLibrary);
-      await expect(
+       await expect( 
         library.connect(otherAccount1).addBook("testing",50))
         .to.revertedWith("Ownable: caller is not the owner");
     });
@@ -54,8 +54,10 @@ describe("Library", function () {
       const [ id ] = transferEvent.args;
       const result = await library.getStock(id);
       expect(result).equal(50);
-    })
+    }) 
+  });
 
+  describe("Borrow books", function (){
     it("Owner borrow a book", async function(){
       const {library} = await loadFixture(deployLibrary);
       const _qty = 50;
@@ -66,6 +68,43 @@ describe("Library", function () {
       await expect(library.borrow(id));
     })
 
-  });
+    it("Cannot borrow a book that doesnt exists", async function(){
+      const {library} = await loadFixture(deployLibrary);
+      const _qty = 50;
+      const txResponse = await library.addBook("testing",_qty);
+      const txReceipt = await txResponse.wait();
+      const [transferEvent] = txReceipt.events;
+      const [ id ] = transferEvent.args;
+      await expect(library.borrow(5));
+    })
+
+    it("Not Owner borrow a book", async function(){
+      const {library,otherAccount1 } = await loadFixture(deployLibrary);
+      const _qty = 50;
+      const txResponse = await library.addBook("testing",_qty);
+      const txReceipt = await txResponse.wait();
+      const [transferEvent] = txReceipt.events;
+      const [ id ] = transferEvent.args;
+      await expect(library.connect(otherAccount1).borrow(id));
+    })
+
+    it("Not Owner cannot borrow a book when it has already borrowed one", async function(){
+      const {library,otherAccount1 } = await loadFixture(deployLibrary);
+      const txResponse = await library.addBook("testing",50);
+      const txReceipt = await txResponse.wait();
+      const  [transferEvent] = txReceipt.events;
+      const [ id1] = transferEvent.args;
+      const txResponse2 = await library.addBook("testing 2",10);
+      const txReceipt2 = await txResponse2.wait();
+      const [transferEvent2] = txReceipt2.events;
+      const [ id2] = transferEvent2.args;
+      await library.connect(otherAccount1).borrow(1);
+      await expect(library.connect(otherAccount1).borrow(2)).to.revertedWith('This Address has already rent a book.');
+    })
+  })
+  
+  describe("Return borrowed books", function (){ 
+
+  })
 
 });
